bf64f0ec631940b8c197e2516e69d6edd063a98e
洪玉婷 Teresa <teresa_hung@systex.com>
2025-02-03 18:00:37 +0800

    bean and @Autowired @Qualifier

---
+++ src/main/java/org/example/demo/CanonPrinter.java
@@ -0,0 +1,11 @@
+package org.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CanonPrinter implements Printer{
+    @Override
+    public void print(String message) {
+        System.out.println("Canon印表機" + message);
+    }
+}
