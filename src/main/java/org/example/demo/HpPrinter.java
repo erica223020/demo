5c0c26c788047420fbb06ee24cb2c754e9fffe5e
洪玉婷 Teresa <teresa_hung@systex.com>
2025-02-04 17:36:48 +0800

    study IoC

--- src/main/java/org/example/demo/HpPrinter.java
+++ src/main/java/org/example/demo/HpPrinter.java
@@ -1,11 +1,23 @@
 package org.example.demo;
 
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer{
+
+    @Value("${printer.name}")
+    private String name;
+
+    @Value("${printer.count:20}") // 加上預設值
+    private int count;
+
     @Override
     public void print(String message) {
-        System.out.println("HP印表機" + message);
+        count--;
+        System.out.println("name:" + name);
+        System.out.println("剩餘使用次數" + count);
     }
 }
--- src/main/java/org/example/demo/MyController.java
+++ src/main/java/org/example/demo/MyController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @Autowired
-    @Qualifier("canonPrinter")
     private Printer printer;
 
     @RequestMapping("/test")
